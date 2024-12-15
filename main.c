cfe30f57fd453f44fc0542173e6f735f78a30a85
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2024-12-15 13:46:57 +0330

    main menu

--- main.c
+++ main.c
@@ -6,14 +6,49 @@
 #include <stdbool.h>  // true, false
 #include <string.h>
 
+#define COMMAND_INPUT 20
+
 
 void clr() {
     system("clear");
 }
 
-int main(int argc, char const *argv[])
-{   
+void printMenu() {
+    printf("\n\n");
+    printf(" 1. buy\n");
+    printf(" 2. exit\n");
+    printf("> ");
+}
+
+void buyMenu() {
+    puts("\tBUY MENU");
+
+}
+
+int main(int argc, char const *argv[]) {   
     clr();
     puts("Hello World!");
+
+    // char comamnd[COMMAND_INPUT];
+
+    while (true) {
+        printMenu();
+        char c = getc(stdin); // todo
+        printf("%c\n", c);
+
+        switch (c)
+        {
+        case 1:
+            buyMenu();
+            break;
+        
+        case 2:
+            break;
+        }
+    }
+    
+
+
+
     return 0;
 }
